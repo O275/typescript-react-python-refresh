371e4faf2de64e28d5407556c8469f59287c3f2d
Ryan <ryanschwarz05@gmail.com>
2025-04-10 09:55:14 -0400

    shape-area logic complete

--- typescript/shapes-area.ts
+++ typescript/shapes-area.ts
@@ -6,9 +6,30 @@ class Circle implements Shape {
   constructor(private radius: number) {}
 
   getArea(): number {
-    return Math.PI * this.radius * this.radius;
+    return Math.PI * this.radius ** 2;
   }
 }
 
+class Rectangle implements Shape {
+  constructor(private length: number, private width: number) {}
+
+  getArea(): number {
+    return this.length * this.width;
+  }
+}
+
+class Triangle implements Shape {
+  constructor(private base:number, private height: number) {}
+
+  getArea(): number {
+    return 0.5 * this.base * this.height;
+  }
+}
 const circle = new Circle(5);
 console.log(`Circle Area: ${circle.getArea()}`);
+
+const rectangle = new Rectangle(5, 10);
+console.log(`Rectangle Area: ${rectangle.getArea()}`);
+
+const triangle = new Triangle(5, 10);
+console.log(`Triangle Area: ${triangle.getArea()}`);
